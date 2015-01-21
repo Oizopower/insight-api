b0fb66d556a90406dc3493092d7d8c1575ed7cfd
Oizopower <oizopower@oizopower.nl>
2015-01-21 19:15:06 +0100

    Changes Whitecoin

---
+++ start.sh
@@ -0,0 +1,15 @@
+export INSIGHT_NETWORK=livenet
+
+export BITCOIND_USER=oizopower
+export BITCOIND_PASS=1234
+export BITCOIND_HOST=127.0.0.1
+export BITCOIND_PORT=15915
+export BITCOIND_P2P_PORT=15914
+
+export INSIGHT_PUBLIC_PATH=public
+export NODE_ENV="production"
+export BITCOIND_DATADIR="/home/insight/.Whitecoin"
+
+export INSIGHT_SAFE_CONFIRMATIONS=0
+
+npm start
